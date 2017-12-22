bd1b59a81df500af7b9ec899973b04d92a35a440
Black13official <34539812+Black13official@users.noreply.github.com>
2017-12-22 14:41:10 +0400

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -1,35 +1,35 @@
 <?php
-
+ 
 try { //Подключение к БД.
-
-$conn = new PDO("sqlsrv:server = tcp:vol1.database.windows.net,1433; Database = Black13", "Danil", "12338110savel");
-
+ 
+$conn = new PDO("sqlsrv:server = tcp:vol1.database.windows.net,1433; Database = Black13", "Danil", "12338110saveL");
+ 
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Создание таблицы в БД
-
+ 
 $sql = "CREATE TABLE registration_tbl(
-
+ 
 id INT NOT NULL IDENTITY(1,1)
-
+ 
 PRIMARY KEY(id),
-
+ 
 name VARCHAR(30),
-
+ 
 email VARCHAR(30),
-
+ 
 date DATE)";
-
+ 
 $conn->query($sql);
-
+ 
 }
-
+ 
 catch (PDOException $e) {
-
+ 
 print("Error connecting to SQL Server.");
-
+ 
 die(print_r($e));
-
+ 
 }
-
+ 
 echo "<h3>Table created.</h3>";
-
-?> 
+ 
+?>
